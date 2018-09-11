39a2e72c4a3175ae40edb47421667b48fb879b85
Srinivasan <srini0593@gmail.com>
2018-09-11 17:55:34 +0530

    TESTMAIN2 CREATED

---
+++ test1/src/io/full/testmain2.java
@@ -0,0 +1,11 @@
+package io.full;
+
+public class testmain2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this from testmain2");
+
+	}
+
+}
