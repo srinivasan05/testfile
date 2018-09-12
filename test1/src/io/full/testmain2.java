e8a78a12c23d803226e77587aa4ebdf0206ec8e2
srinivasan05 <42633045+srinivasan05@users.noreply.github.com>
2018-09-12 16:17:18 +0530

    Update testmain2.java

--- test1/src/io/full/testmain2.java
+++ test1/src/io/full/testmain2.java
@@ -5,6 +5,7 @@ public class testmain2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("from testmain2");
+		System.out.println("This from the sample branch by rajesh side.");
 
 	}
 
