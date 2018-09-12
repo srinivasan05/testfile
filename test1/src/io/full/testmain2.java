51981bb50916502eb1941706fa4b7b93114d9035
Srinivasan <srini0593@gmail.com>
2018-09-12 15:32:20 +0530

    v1

--- test1/src/io/full/testmain2.java
+++ test1/src/io/full/testmain2.java
@@ -4,7 +4,7 @@ public class testmain2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this from testmain2");
+		System.out.println("from testmain2");
 
 	}
 
