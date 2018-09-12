0331208f4c16dcc833e826bf6b83e2b431db3e62
srinivasan05 <42633045+srinivasan05@users.noreply.github.com>
2018-09-12 16:01:41 +0530

    Update testmain2.java

--- test1/src/io/full/testmain2.java
+++ test1/src/io/full/testmain2.java
@@ -5,6 +5,7 @@ public class testmain2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("from testmain2");
+		System.out.println("new creating from network");
 
 	}
 
